cfda150bb6f10073cabc06d56ca7e111ec61e4bc
Malek Smida <msmida96@gmail.com>
2020-09-22 14:51:08 +0100

    #Add: app header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([
+    { name: "worldwide" },
+    { name: "tunisia" },
+  ]);
   return (
     <div className="app">
-      <h1>hello!</h1>
+      <div className="app__header">
+        <h1>Covid 19 Update</h1>
+        <FormControl className="app__header__dropdown">
+          <Select variant="outlined" value={countries[0].name}>
+            {countries.map((country) => (
+              <MenuItem value={country.name}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* <Header /> */}
 
       {/* <InfoBox/> */}
